2008466138d36cba132d0d0e135b45503c5dd305
natanaeladit <nata.adit@gmail.com>
2022-03-12 14:20:39 +0800

    add authorized controller

---
+++ server/Controllers/MessagesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OpenIddict.Validation.AspNetCore;
+
+namespace server.Controllers
+{
+    [Authorize(AuthenticationSchemes = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        public JsonResult Get()
+        {
+            return new JsonResult(new List<string>() { "Welcome", "Thank you" });
+        }
+    }
+}
